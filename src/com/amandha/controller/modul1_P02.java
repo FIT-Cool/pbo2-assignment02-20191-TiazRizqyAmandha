6907800fdac0018e6f6417e4be8be413d81e4fb1
TiazRizqyAmandha <amandhatiazrizqi@gmail.com>
2019-09-16 10:06:57 +0700

    update belum jadi
    karena kalau nama dan harga kosong belum muncul errornya

--- src/com/amandha/controller/modul1_P02.java
+++ src/com/amandha/controller/modul1_P02.java
@@ -25,9 +25,11 @@ public class modul1_P02 implements Initializable {
     @FXML
     private TableView<Items> tbView;
     @FXML
-    private TableColumn<Items, String> col01;
     private ObservableList<Items> items;
+    @FXML
     private ObservableList<Category> categories;
+    @FXML
+    private TableColumn<Items, String> col01;
     @FXML
     private TableColumn<Items, Double> col02;
     @FXML
@@ -44,88 +46,97 @@ public class modul1_P02 implements Initializable {
 
     @FXML
     private void SaveAction(ActionEvent actionEvent) {
-        Items i = new Items();
-        simpan.setDisable(false);
-        update.setDisable(true);
-        int count = (int)categories.stream().filter(p -> p.getNama().equalsIgnoreCase(txtName.getText())).count();
-        i.setNama(txtName.getText().trim());
-        i.setHarga(Double.parseDouble(txtPrice.getText()));
-        i.setCategory(ComboBoxCat.getValue());
-        items.add(i);
-        txtName.clear();
-        txtPrice.clear();
+        Alert a = new Alert(Alert.AlertType.ERROR);
+        if(txtName.getText() == null || txtPrice.getText() == null || ComboBoxCat.getValue() == null) {
+            a.setContentText("Please fill name/ price/ category");
+            a.showAndWait();
+        }
+        else{
+            int count = (int) items.stream().filter(p -> p.getNama().equalsIgnoreCase(txtName.getText())).count();
+            if(count >= 1){
+                a.setContentText("Duplicate name");
+                a.showAndWait();
+            }
+            else{
+                Items i = new Items();
+                i.setNama(txtName.getText().trim());
+                i.setHarga(Double.parseDouble(txtPrice.getText().trim()));
+                i.setCategory(ComboBoxCat.getValue());
+                items.add(i);
+            }
+        }
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        items = FXCollections.observableArrayList();
-        tbView.setItems(items);
         categories = FXCollections.observableArrayList();
         ComboBoxCat.setItems(categories);
+        items = FXCollections.observableArrayList();
+        tbView.setItems(items);
         col01.setCellValueFactory(data -> {
-            Items i = data.getValue();
-            return new SimpleStringProperty(i.getNama());
+            Items a = data.getValue();
+            return new SimpleStringProperty(a.getNama());
         });
         col02.setCellValueFactory(data -> {
-            Items i = data.getValue();
-            return new SimpleDoubleProperty(i.getHarga()).asObject();
+            Items b = data.getValue();
+            return new SimpleDoubleProperty(b.getHarga()).asObject();
         });
         col03.setCellValueFactory(data -> {
-            Items i = data.getValue();
-            return new SimpleStringProperty(i.getCategory().getNama());
+            Items c = data.getValue();
+            return new SimpleStringProperty(c.getCategory().getNama());
         });
-        update.setDisable(true);
     }
 
 
     @FXML
     private void ResetAction(ActionEvent actionEvent) {
         simpan.setDisable(false);
-        reset.setDisable(false);
         update.setDisable(true);
-        txtName.clear();
-        txtPrice.clear();
-        txtCN.clear();
+        txtName.setText(null);
+        txtPrice.setText(null);
+        txtCN.setText(null);
         ComboBoxCat.setValue(null);
     }
 
     @FXML
     private void UpdateAction(ActionEvent actionEvent) {
-        simpan.setDisable(true);
-        int count = (int)categories.stream().filter(p -> p.getNama().equalsIgnoreCase(txtName.getText())).count();
-        Items i = new Items();
-        i.setNama(txtName.getText().trim());
-        i.setHarga(Double.parseDouble(txtPrice.getText()));
+        Items i = tbView.getSelectionModel().getSelectedItem();
         i.setCategory(ComboBoxCat.getValue());
-
-        i.setNama(txtName.getText().trim());
+        i.setNama(txtName.getText());
         i.setHarga(Double.parseDouble(txtPrice.getText()));
-        i.setCategory(ComboBoxCat.getValue());
-
         tbView.refresh();
-
     }
 
     @FXML
     private void SCAction(ActionEvent actionEvent) {
-        Category cat = new Category();
-        cat.setNama(txtCN.getText().trim());
-        categories.add(cat);
+        Alert a = new Alert(Alert.AlertType.ERROR);
+        if(txtCN.getText().isEmpty()){
+            a.setContentText("Please fill category name");
+            a.showAndWait();
+        }
+        else {
+            Category c = new Category();
+            c.setNama(txtCN.getText().trim());
+            int count = (int) categories.stream().filter(p -> p.getNama().equalsIgnoreCase(txtCN.getText().trim())).count();
+            if(count >= 1){
+                a.setContentText("Duplicate category name");
+                a.showAndWait();
+            }
+            else{
+                categories.add(c);
+            }
+        }
 
     }
 
     @FXML
     private void tableClicked(MouseEvent mouseEvent) {
-        Items selectedItems = tbView.getSelectionModel().getSelectedItem();
-        if(selectedItems !=null){
-            txtName.setText(tbView.getSelectionModel().getSelectedItem().getNama());
-            txtPrice.setText(String.valueOf(tbView.getSelectionModel().getSelectedItem().getHarga()));
-            ComboBoxCat.setValue(tbView.getSelectionModel().getSelectedItem().getCategory());
-            simpan.setDisable(true);
-            update.setDisable(false);
-        }
-        //Fakultas f = tableDepartment.getSelectionModel().getSelectedItem();
-        //txtName.setText(f.getName());
+        Items i = tbView.getSelectionModel().getSelectedItem();
+        txtName.setText(i.getNama());
+        txtPrice.setText(String.valueOf(i.getHarga()));
+        ComboBoxCat.setValue(i.getCategory());
+        simpan.setDisable(true);
+        update.setDisable(false);
 
     }
 }
